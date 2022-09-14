fb60a6fc12ec5dfa94c92df63e51ab7abb0b8229
Leonardo <leonardobelilo@outlook.com>
2022-09-13 21:27:41 -0300

    start app baseoutside

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [statement,setStatement] = useState(false)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Baseoutside</Text>
+      <Button title='click aqui por favor ' onPress={()=>setStatement(!statement)}/>
+        {!statement && <Text style={ styles.statement}>Hellow my friend </Text>}
+        
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +17,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:42,
+    fontWeight:'bold'
+  },
+  statement:{
+    fontSize:24
+  }
 });
